05a72badddb96bf8094d873e3af4651bb7040491
CJuice <CJuice@users.noreply.github.com>
2019-12-03 13:08:18 -0500

    Update doit_WA_machines_logfile_bloat_reduction.py

--- doit_WA_machines_logfile_bloat_reduction.py
+++ doit_WA_machines_logfile_bloat_reduction.py
@@ -1,9 +1,17 @@
 """
+Walk the designated folder, assess age of files, delete those older than the acceptable age.
 
+Created to address log file bloat on the D drive on the web adapter machines. This script walks the log files folder.
+It hits the four sub-folders within the log file folder. The files in each of the sub-folders is assessed for its age
+and deleted or passed.
+Author: CJuice
+Created: 20191203
+Revisions:
 """
 
 
 def main():
+
     # IMPORTS
     import os
     import datetime
@@ -25,7 +33,7 @@ def main():
                 is_older_than_acceptable_age = duration_since_file_last_modified > acceptable_age_days
                 if is_older_than_acceptable_age:
                     try:
-                        # os.remove(full_file_path)
+                        os.remove(full_file_path)
                         print("FILE: {} REMOVED. Age: {}".format(full_file_path, duration_since_file_last_modified))
                     except Exception as e:
                         print("\tALERT: {} NOT REMOVED. EXCEPTION! {}".format(full_file_path, e))

aa9b528e6cee4dc8cde733616d075c112984f992
CJuice <CJuice@users.noreply.github.com>
2019-12-03 12:50:22 -0500

    Update doit_WA_machines_logfile_bloat_reduction.py

--- doit_WA_machines_logfile_bloat_reduction.py
+++ doit_WA_machines_logfile_bloat_reduction.py
@@ -5,8 +5,14 @@
 
 def main():
     # IMPORTS
+    import os
+    import datetime
 
     # VARIABLES
+    # log_file_folders_path= r"D:\inetpub\logs\LogFiles"  # PRODUCTION
+    log_file_folders_path= r"C:\Users\Conrad.Schaefer\Documents\DoIT_WAmachinesLogFileBloatReduction\LogFileBloatReduction_WAmachines\W3SVC1_test"  # TESTING
+    acceptable_age_days = datetime.timedelta(days=90)
+    now = datetime.datetime.now()
 
     # ASSERTS
     
@@ -15,7 +21,28 @@ def main():
     # CLASSES
 
     # FUNCTIONALITY
+    try:
+        for root, dirnames, files in os.walk(log_file_folders_path):
+            for file in files:
 
+                # For files in the directory, process them.
+                full_file_path = os.path.join(root, file)
+                time_file_last_modified = os.path.getmtime(full_file_path)
+                duration_since_file_last_modified = now - datetime.datetime.fromtimestamp(time_file_last_modified)
+                is_older_than_acceptable_age = duration_since_file_last_modified > acceptable_age_days
+                if is_older_than_acceptable_age:
+                    try:
+                        os.remove(full_file_path)
+                        print("FILE: {} REMOVED. Age: {}".format(full_file_path, duration_since_file_last_modified))
+                    except Exception as e:
+                        print("\tALERT: {} NOT REMOVED. EXCEPTION! {}".format(full_file_path, e))
+
+    except IOError as io_err:
+        print(io_err)
+        exit()
+    except Exception as e:
+        print(e)
+        exit()
     return
 
 

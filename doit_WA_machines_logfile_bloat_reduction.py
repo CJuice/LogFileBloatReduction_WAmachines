e8e81235cd72bba852045b9e487816b0fa0fc4a2
CJuice <CJuice@users.noreply.github.com>
2019-12-03 13:11:57 -0500

    Update doit_WA_machines_logfile_bloat_reduction.py

--- doit_WA_machines_logfile_bloat_reduction.py
+++ doit_WA_machines_logfile_bloat_reduction.py
@@ -4,6 +4,7 @@ Walk the designated folder, assess age of files, delete those older than the acc
 Created to address log file bloat on the D drive on the web adapter machines. This script walks the log files folder.
 It hits the four sub-folders within the log file folder. The files in each of the sub-folders is assessed for its age
 and deleted or passed.
+WA machines do not have visual cron so this script will need to be run manually.
 Author: CJuice
 Created: 20191203
 Revisions:
